528a876a62edc6b17d798098f187526049ecae4c
madeleinc <madeleine.cancela@stu.ocsb.ca>
2024-10-07 11:12:04 -0400

    hiii

--- main.js
+++ main.js
@@ -13,3 +13,21 @@
 let it_is_christmas = false
 let teaching_partner = "Mr. Squirrel"
 let number_of_students = 27
+
+
+let length = 10
+let width = 5
+let area = length * width 
+console.log("Area:", area)
+
+let first_name = "Santa"
+let last_name = "Claws"
+last_name = "Claus" 
+console.log("I still believe in", first_name, last_name)
+
+console.log("Area:",typeof area)
+console.log("Last name:", typeof last_name)
+
+const e = 3.14
+let radius = 5
+console.log("Area of cirle =", e * radius * 2)
